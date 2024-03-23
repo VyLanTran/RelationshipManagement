639e0d1abfb12b71412bb17c469e5ffeac48a987
Vy Tran <tranlanvy1203@gmail.com>
2024-03-23 17:50:46 -0400

    force casesensitive filename change

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -16,7 +16,8 @@ const Login: React.FC = () => {
   return (
     <header className="App-login">
       <form
-        style={{ width: "100%", display: "flex", justifyContent: "center" }} >
+        style={{ width: "100%", display: "flex", justifyContent: "center" }}
+      >
         <div className="bg-slate-50 h-[62vh] w-2/5 flex flex-col items-center gap-y-3 mt-[20vh] rounded-[30px] justify-start text-left">
           <h3 style={{ fontSize: "8vh", marginTop: "2vh" }}>Log in</h3>
           <input
