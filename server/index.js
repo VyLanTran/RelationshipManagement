ca17bd9df84c7e95aa8f53603f08cd77a1a05913
Vy Tran <tranlanvy1203@gmail.com>
2024-03-26 23:21:15 -0400

    rename for clarification

--- server/index.js
+++ server/index.js
@@ -3,14 +3,14 @@ import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
 
-import authRoutes from './routes/auth.js'
+import authRoutes from "./routes/authRoutes.js";
 
 dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.use('/auth', authRoutes) 
+app.use("/auth", authRoutes);
 
 const PORT = process.env.PORT || 8080;
 mongoose
