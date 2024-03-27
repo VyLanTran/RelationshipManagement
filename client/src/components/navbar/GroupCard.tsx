b252f68d4dd8f79dba9eea7ba48ccf99e081cc73
Tran Trang Linh <trantranglinh231104@gmail.com>
2024-03-27 12:34:17 -0400

    Adding frontend (showing the group) in the homepage with dummy group

---
+++ client/src/components/navbar/GroupCard.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Link } from 'react-router-dom'
+
+// TODO: show name when hovering over icon
+interface GroupCardProps {
+    group_name: string
+    url: string
+    participants: string[]
+}
+
+const GroupCard: React.FC<GroupCardProps> = ({ group_name, participants, url }) => {
+    return (
+        <div className="bg-[#FFB302] rounded-[20px] w-[40vh] h-[40vh] p-[2.5vh] m-[2vh] hover:cursor-pointer hover:text-[white] hover:bg-[rgb(59,59,59)]">
+            <div>
+                <Link to={url} title={group_name}>
+                <p className="text-3xl pb-[2.5vh]"> {group_name} </p>
+                </Link>
+            </div>
+            <div className="text-left px-[3vh]">
+                {participants.map(participant => <p className="text-xl">{participant}</p>)}
+            </div>
+        </div>        
+    );
+}
+
+export default GroupCard;
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,11 +1,27 @@
 import React from "react";
 import Navbar from "../components/navbar/Navbar.tsx";
+import GroupCard from "../components/navbar/GroupCard.tsx";
 
 const Home = () => {
     return (
         <div>
             <Navbar />
-            <div className="pt-[60px]">This is home page</div>
+            <div className="text-center pt-[90px]">
+                <h1 className="text-4xl">Discover your friend groups.</h1>
+            </div>
+            <div className="flex justify-center flex-row pt-[20px]">
+                <GroupCard group_name={"something"} url={"something"} participants={["something", "something"]} />
+                <GroupCard group_name={"something"} url={"something"} participants={["something", "something", "somethingelse"]} />
+                <GroupCard group_name={"something"} url={"something"} participants={["something", "something"]} />
+            </div>
+            <div>
+                <input className="bg-[#FFB302] text-xl text-center w-[100vh] text-[large] h-[7vh] mb-[-2vh] mt-[1vh] rounded-[10px] hover:cursor-pointer hover:text-[white] hover:bg-[rgb(59,59,59)]"
+                    value="See all of your friend groups."
+                />
+            </div>
+            <div className="mt-[3vh]">
+                <p className="text-l text-gray-700">...or, discover common friends by clicking outside the group</p>
+            </div>
         </div>
     );
 }
