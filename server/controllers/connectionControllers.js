f409f1eaa8c6b3934792b53e67f92d4b322ed6d5
Jimmykhangnguyen <jimmykhangnguyen@gmail.com>
2024-03-27 22:39:02 -0400

    Get Specific Connection Backend

--- server/controllers/connectionControllers.js
+++ server/controllers/connectionControllers.js
@@ -17,3 +17,15 @@ export const createConnection = async (req, res) => {
 		res.status(500).json({ msg: error });
 	}
 };
+
+export const getConnection = async (req, res) => {
+	try {
+		const { _id } = req.body;
+
+		const connection = await Connection.findOne({ _id: _id });
+		res.status(201).json({ connection });
+	}
+	catch (error) {
+		res.status(500).json({ msg: error });
+	}
+};
