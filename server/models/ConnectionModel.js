6bc56183e078dd53b0d99dad449865081a3c7041
dtnlong1 <dtnlong04@gmail.com>
2024-03-27 20:14:25 -0500

    update connectionSchema

--- server/models/ConnectionModel.js
+++ server/models/ConnectionModel.js
@@ -1,13 +1,37 @@
 import mongoose from "mongoose";
+import User from "./UserModel";
 
 const ConnectionSchema = new mongoose.Schema({
-	firstName: {
+	author: {
+		type: User,
+		required: true,
+	},
+	name: {
 		type: String,
 		required: true,
 	},
-	lastName: {
+	phone: {
 		type: String,
-		required: true,
+	},
+	email: {
+		type: String,
+	},
+	username: {
+		type: String,
+	},
+	birthday: {
+		type: Date,
+	},
+	avatar: {
+		type: Image,
+	},
+	fun_facts: {
+		type: Array,
+		default: [],
+	},
+	others: {
+		type: Array,
+		default: [],
 	},
 });
 const ConnectionModel = mongoose.model("Connection", ConnectionSchema);
