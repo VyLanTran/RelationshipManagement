76d3e932748b846e998a5eec03592b02b50da4e3
Tran Trang Linh <trantranglinh231104@gmail.com>
2024-03-28 22:09:27 -0400

    Adding feature to delete group and show one group

--- server/controllers/groupControllers.js
+++ server/controllers/groupControllers.js
@@ -2,18 +2,40 @@ import Group from "../models/GroupModel.js";
 
 export const addGroup = async (req, res) => {
     try {
-      const savedGroup = await Group.create(req.body);
-      res.status(201).json({ savedGroup });
+        const savedGroup = await Group.create(req.body);
+        res.status(201).json({ savedGroup });
     } catch (err) {
-      res.status(500).json({ error: err.message });
+        res.status(500).json({ error: err.message });
     }
 };
 
 export const showAll = async (req, res) => {
     try {
         const groups = await Group.find({});
-		res.status(200).json( groups );
+        res.status(200).json(groups);
     } catch (err) {
         res.status(500).json({ error: err.message });
     }
-}
+};
+
+export const deleteGroup = async (req, res) => {
+    try {
+        const { id: groupId } = req.params;
+
+		const group = await Group.deleteOne({ _id: groupId });
+		res.status(201).json({ group });
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+};
+
+export const showGroup = async (req, res) => {
+    try {
+        const { id: groupId } = req.params;
+
+		const group = await Group.findOne({ _id: groupId });
+		res.status(201).json({ group });
+    } catch (err) {
+        res.status(404).json({ error: err.message });
+    }
+};
