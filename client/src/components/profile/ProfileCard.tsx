5a8318cd3f7bdeda9392e10b93b526eff4574243
Vy Tran <tranlanvy1203@gmail.com>
2024-04-01 14:02:06 -0400

    add some new files for Profile page

---
+++ client/src/components/profile/ProfileCard.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const ProfileCard = () => {
+    return (
+        <div className='w-full px-4 bg-lime-300'>
+            {/* Background image and avatar */}
+            <div className='bg-red-200 h-20'>
+
+            </div>
+        </div>
+    );
+}
+
+export default ProfileCard;
--- client/src/pages/Profile.tsx
+++ client/src/pages/Profile.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import Navbar from "../components/navbar/Navbar.tsx";
+import ProfileCard from "../components/profile/ProfileCard.tsx";
+import { useAuthContext } from "../hooks/useAuthContext.js";
 
 const Profile = () => {
+    const { user } = useAuthContext();
+
     return (
         <div>
             <Navbar />
-            <div className="pt-[60px]">This is profile page</div>
+            <ProfileCard />
         </div>
     );
 }
