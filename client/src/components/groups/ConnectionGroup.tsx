15cf91cd72f43c32d9b1b19613c05dfb8a9a5ff9
Jimmykhangnguyen <jimmykhangnguyen@gmail.com>
2024-04-04 14:50:08 -0400

    Ongoing Connections

--- client/src/components/groups/ConnectionGroup.tsx
+++ client/src/components/groups/ConnectionGroup.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from 'react-router-dom'
+import Select from 'react-select'
 
 // TODO: show name when hovering over icon
 interface GroupCardProps {
@@ -9,8 +10,15 @@ interface GroupCardProps {
 }
 
 const ConnectionGroup: React.FC<GroupCardProps> = ({ group_name, participants, url }) => {
+    const options: { value: string, label: string }[] = [
+        { value: "Viet Tech", label: "Viet Tech" },
+        { value: "Team 4", label: "Team 4" },
+        { value: "12CA1", label: "12CA1" }
+    ];
+    
     return (
         <div className="bg-[#FFB302] rounded-[20px] w-[50vh] h-[84vh] p-[2.5vh] m-[2vh]">
+            <Select options={options} />
             <div>
                 <p className="text-3xl pb-[2.5vh]"> {group_name} </p>
             </div>
--- client/src/pages/Connection.tsx
+++ client/src/pages/Connection.tsx
@@ -14,24 +14,24 @@ const Connection = () => {
                     url={""}
                     participants={["something", "something"]} // Placeholder for now
                 />
-                <div className="w-[150vh] rounded-[20px] h-[84vh] p-[1vh] m-[2vh]">
+                <div className="w-[145vh] rounded-[20px] h-[84vh] p-[1vh] m-[2vh]">
                     <form className="flex justify-between">
                         <div className="flex">
                             <input className="border border-solid border-[rgb(84,84,84)] bg-slate-50 w-[40vh] h-[6vh] outline-none rounded-3xl pl-[2vh]" placeholder="Finding someone?"></input>
                             <button className="font-azeret bg-slate-50 w-[25vh] text-[large] font-bold border h-[6vh] rounded-3xl ml-[2vh] border-solid border-[rgb(84,84,84)] hover:cursor-pointer hover:text-[white] hover:bg-[rgb(59,59,59)]">Search</button>
                         </div>
-                        <button className="font-azeret bg-slate-50 w-[20vh] text-[large] font-bold border h-[6vh] rounded-3xl ml-[2vh] border-solid border-[rgb(84,84,84)] hover:cursor-pointer hover:text-[white] hover:bg-[rgb(59,59,59)]">Add</button>
+                        <button className="font-azeret bg-[#8DC363] w-[20vh] text-[large] font-bold border h-[6vh] rounded-3xl ml-[2vh] border-solid border-[rgb(84,84,84)] hover:cursor-pointer hover:text-[white] hover:bg-[rgb(59,59,59)]">Add</button>
                     </form>
                     <section className="h-[75vh] mt-[2vh] rounded-[20px]">
                         <div className="flex justify-between">
                             <ConnectionCard />
                             <ConnectionCard />
                         </div>
-                        <div className="flex justify-between mt-[9vh]">
+                        <div className="flex justify-between mt-[4vh]">
                             <ConnectionCard />
                             <ConnectionCard />
                         </div>
-                        <div className="flex justify-between mt-[9vh]">
+                        <div className="flex justify-between mt-[4vh]">
                         <ConnectionCard />
                             <ConnectionCard />
                         </div>
