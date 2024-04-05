6ab93bcc80b883b2236f4e8486a4d1c4a416726b
Jimmykhangnguyen <jimmykhangnguyen@gmail.com>
2024-04-05 10:27:08 -0400

    Ongoing Connections

--- client/src/pages/Connection2.tsx
+++ client/src/pages/Connection2.tsx
@@ -32,34 +32,14 @@ const Connection = () => {
             fetchConnections();
         }
     }, [connections, user]);
+
+
     
     return (
         <div>
             <Navbar />
-                {connections.length == 0 ?
-                <div className="flex justify-center pt-[10vh]">
-                    <ConnectionGroup />
-                    <div className="w-[145vh] rounded-[20px] h-[84vh] p-[1vh] m-[2vh]">
-                    </div>
-                </div>
-                :
-                <div className="flex justify-center pt-[10vh]">
-                    <ConnectionGroup />
-                    <div className="w-[145vh] rounded-[20px] h-[84vh] p-[1vh] m-[2vh]">
-                        <form className="flex justify-between">
-                            <div className="flex">
-                                <input className="border border-solid border-[rgb(84,84,84)] bg-slate-50 w-[40vh] h-[6vh] outline-none rounded-3xl pl-[2vh]" placeholder="Finding someone?"></input>
-                                <button className="font-azeret bg-slate-50 w-[25vh] text-[large] font-bold border h-[6vh] rounded-3xl ml-[2vh] border-solid border-[rgb(84,84,84)] hover:cursor-pointer hover:text-[white] hover:bg-[rgb(59,59,59)]">Search</button>
-                            </div>
-                            <button className="font-azeret bg-[#8DC363] w-[20vh] text-[large] font-bold border h-[6vh] rounded-3xl ml-[2vh] border-solid border-[rgb(84,84,84)] hover:cursor-pointer hover:text-[white] hover:bg-[rgb(59,59,59)]">Add</button>
-                        </form>
-                        <section className="h-[75vh] mt-[2vh] rounded-[20px]">
-                            
-                        </section>
-                    </div>
-                </div>
-                }
-                {/* <ConnectionGroup />
+            <div className="flex justify-center pt-[10vh]">
+                <ConnectionGroup />
                 <div className="w-[145vh] rounded-[20px] h-[84vh] p-[1vh] m-[2vh]">
                     <form className="flex justify-between">
                         <div className="flex">
@@ -103,8 +83,9 @@ const Connection = () => {
                         </div>
 
                     </section>
-                </div> */}
+                </div>
             </div>
+        </div>
     );
 };
 
