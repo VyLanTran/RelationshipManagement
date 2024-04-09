ce3bda4cf6bc8b9347c97ba708518686d70de82a
dtnlong1 <dtnlong04@gmail.com>
2024-04-08 22:16:57 -0500

    add and update connection

---
+++ client/src/components/groups/ConnectionCard.jsx
@@ -0,0 +1,106 @@
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { MdEdit } from "react-icons/md";
+import { FaTrashAlt } from "react-icons/fa";
+import { IoPersonCircleSharp } from "react-icons/io5";
+import EditModal from "../connection/EditModal.jsx";
+import { useSelector } from "react-redux";
+
+// interface ConnectionCardProps {
+// 	_id: object;
+// 	name: string;
+// 	member_of: string;
+// 	phone: string;
+// 	email: string;
+// 	last_contacted: string;
+// }
+
+const ConnectionCard = ({
+	_id,
+	name,
+	member_of,
+	phone,
+	email,
+	last_contacted,
+}) => {
+	const [isModalOpen, setIsModalOpen] = useState(false);
+	const token = useSelector((state) => state.auth.token);
+	const [connection, setConnection] = useState(null);
+
+	const currentConnection = useSelector((state) => state.auth.user);
+
+	const handleClick = async (e) => {
+		e.preventDefault();
+
+		const res = await fetch("http://localhost:3001/connections/" + _id, {
+			method: "DELETE",
+			headers: { "Content-Type": "application/json" },
+		});
+	};
+
+	useEffect(() => {
+		const getConnection = async () => {
+			const res = await fetch(`http://localhost:3001/connection/${_id}`, {
+				method: "GET",
+				headers: { Authorization: `Bearer ${token}` },
+			});
+			const data = await res.json();
+
+			if (res.ok) {
+				setConnection(data);
+			}
+		};
+		getConnection();
+	}, [connection]);
+
+	const handleEditClick = () => {
+		setIsModalOpen(true);
+	};
+
+	const handleCloseModal = () => {
+		setIsModalOpen(false);
+	};
+
+	return (
+		<div className="h-[21vh] w-[67vh] bg-white rounded-[20px] flex mb-[4vh]">
+			<IoPersonCircleSharp className="ml-[2vh] mt-[2.5vh] w-[16vh] h-[16vh]" />
+			<div className="mt-[1vh] w-[43vh]">
+				<h2 className="font-bold text-left text-[3.5vh] ml-[2vh]">{name}</h2>
+				<div className="text-left ml-[2vh] text-[2.2vh]">
+					<p>
+						<b>Groups in:</b> {member_of}
+					</p>
+					<p>
+						<b>Phone:</b> {phone}
+					</p>
+					<p>
+						<b>Email:</b> {email}
+					</p>
+					<p>
+						<b>Last contacted:</b> {last_contacted}
+					</p>
+				</div>
+			</div>
+			<div className="w-[8vh] h-[21vh] rounded-[20px] flex  flex-col items-center justify-around pt-[2vh] pb-[2vh] mr-[0.5vh]">
+				<button
+					className="flex items-center justify-center font-azeret bg-[#FFB302] w-[6vh] text-[4vh] font-bold border h-[6vh] rounded-2xl border-solid border-[rgb(84,84,84)] hover:cursor-pointer hover:text-[white] hover:bg-[rgb(59,59,59)]"
+					onClick={handleEditClick}>
+					<MdEdit />
+				</button>
+				<EditModal
+					connection={connection}
+					connectionId={_id}
+					isOpen={isModalOpen}
+					onClose={handleCloseModal}
+				/>
+				<button
+					onClick={handleClick}
+					className="flex items-center justify-center font-azeret bg-[#F85555] w-[6vh] text-[4vh] font-bold border h-[6vh] rounded-2xl border-solid border-[rgb(84,84,84)] hover:cursor-pointer hover:text-[white] hover:bg-[rgb(59,59,59)]">
+					<FaTrashAlt />
+				</button>
+			</div>
+		</div>
+	);
+};
+
+export default ConnectionCard;
--- client/src/components/users/About.jsx
+++ client/src/components/users/About.jsx
@@ -4,102 +4,104 @@ import EditModal from "./EditModal.jsx";
 import { useSelector } from "react-redux";
 
 const About = ({ userId }) => {
-    // this user is myself, who is authenticated to use this app
-    const currentUser = useSelector((state) => state.auth.user)
-    const token = useSelector((state) => state.auth.token)
-    const [user, setUser] = useState(null)
+	// this user is myself, who is authenticated to use this app
+	const currentUser = useSelector((state) => state.auth.user);
+	const token = useSelector((state) => state.auth.token);
+	const [user, setUser] = useState(null);
 
-    const [isModalOpen, setIsModalOpen] = useState(false);
+	const [isModalOpen, setIsModalOpen] = useState(false);
 
-    // fetch the data of this profile from the database once when we load this page
-    useEffect(() => {
-        const getUser = async () => {
-            const res = await fetch(`http://localhost:3001/users/${userId}`, {
-                method: 'GET',
-                headers: { 'Authorization': `Bearer ${token}` },
-            })
-            const data = await res.json()
+	// fetch the data of this profile from the database once when we load this page
+	useEffect(() => {
+		const getUser = async () => {
+			const res = await fetch(`http://localhost:3001/users/${userId}`, {
+				method: "GET",
+				headers: { Authorization: `Bearer ${token}` },
+			});
+			const data = await res.json();
 
-            if (res.ok) {
-                setUser(data)
-            }
-        }
+			if (res.ok) {
+				setUser(data);
+			}
+		};
 
-        // only allow currentUser to read this profile if they are authenticated
-        if (currentUser) {
-            getUser()
-        }
-    }, [user])
+		// only allow currentUser to read this profile if they are authenticated
+		if (currentUser) {
+			getUser();
+		}
+	}, [user]);
 
-    const handleEditClick = () => {
-        setIsModalOpen(true);
-    };
+	const handleEditClick = () => {
+		setIsModalOpen(true);
+	};
 
-    const handleCloseModal = () => {
-        setIsModalOpen(false);
-    };
+	const handleCloseModal = () => {
+		setIsModalOpen(false);
+	};
 
-    return (
-        <div>
-            {
-                !user ? <div>No User</div> :
+	return (
+		<div>
+			{!user ? (
+				<div>No User</div>
+			) : (
+				<div className="grid grid-cols-1 md:grid-cols-2 text-[14px] relative">
+					{/* TODO: Set secret information */}
+					<div className="p-4 justify-start flex border">
+						Live in
+						<span className="font-bold ml-1">
+							{user.currentCity ? user.currentCity : ""}
+						</span>
+					</div>
+					<div className="p-4 justify-start flex border">
+						Came from
+						<span className="font-bold ml-1">
+							{user.hometown ? user.hometown : ""}
+						</span>
+					</div>
+					<div className="p-4 justify-start flex border">
+						Work at
+						<span className="font-bold ml-1">
+							{user.company ? user.company : ""}
+						</span>
+					</div>
+					<div className="p-4 justify-start flex border">
+						Study at
+						<span className="font-bold ml-1">
+							{user.school ? user.school : ""}
+						</span>
+					</div>
+					<div className="p-4 justify-start flex border">
+						Phone
+						<span className="font-bold ml-1">
+							{user.phone ? user.phone : ""}
+						</span>
+					</div>
+					<div className="p-4 justify-start flex border">
+						Email
+						<span className="font-bold ml-1">
+							{user.email ? user.email : ""}
+						</span>
+					</div>
 
-                    <div className="grid grid-cols-1 md:grid-cols-2 text-[14px] relative">
-                        {/* TODO: Set secret information */}
-                        <div className="p-4 justify-start flex border">
-                            Live in
-                            <span className="font-bold ml-1">
-                                {user.currentCity ? user.currentCity : ""}
-                            </span>
-                        </div>
-                        <div className="p-4 justify-start flex border">
-                            Came from
-                            <span className="font-bold ml-1">
-                                {user.hometown ? user.hometown : ""}
-                            </span>
-                        </div>
-                        <div className="p-4 justify-start flex border">
-                            Work at
-                            <span className="font-bold ml-1">
-                                {user.company ? user.company : ""}
-                            </span>
-                        </div>
-                        <div className="p-4 justify-start flex border">
-                            Study at
-                            <span className="font-bold ml-1">
-                                {user.school ? user.school : ""}
-                            </span>
-                        </div>
-                        <div className="p-4 justify-start flex border">
-                            Phone
-                            <span className="font-bold ml-1">
-                                {user.phone ? user.phone : ""}
-                            </span>
-                        </div>
-                        <div className="p-4 justify-start flex border">
-                            Email
-                            <span className="font-bold ml-1">
-                                {user.email ? user.email : ""}
-                            </span>
-                        </div>
+					{/* You are only allowed to edit if this is your profile page */}
+					{currentUser._id === userId && (
+						<div
+							onClick={handleEditClick}
+							className="absolute top-0 right-0 transform translate-x-1/4 -translate-y-1/4 bg-[#FFB302] w-8 h-8 rounded-full flex items-center justify-center">
+							<MdOutlineEdit size={20} />
+						</div>
+					)}
 
-                        {/* You are only allowed to edit if this is your profile page */}
-                        {currentUser._id === userId &&
-                            <div
-                                onClick={handleEditClick}
-                                className="absolute top-0 right-0 transform translate-x-1/4 -translate-y-1/4 bg-[#FFB302] w-8 h-8 rounded-full flex items-center justify-center">
-                                <MdOutlineEdit size={20} />
-                            </div>}
-
-                        <EditModal
-                            user={user}
-                            userId={userId}
-                            isOpen={isModalOpen}
-                            onClose={handleCloseModal} />
-                    </div>
-            }
-        </div>
-    );
-}
+					<EditModal
+						user={user}
+						userId={userId}
+						isOpen={isModalOpen}
+						onClose={handleCloseModal}
+					/>
+				</div>
+			)}
+		</div>
+	);
+};
 
 export default About;
