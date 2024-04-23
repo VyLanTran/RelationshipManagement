9ecb5623e1cd77ded61e3d16f1f513344d993668
Vy Tran <tranlanvy1203@gmail.com>
2024-04-23 16:10:19 -0400

    login with third-parties

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,22 +1,29 @@
-import React, { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
-import { Icon } from "react-icons-kit";
-import { eyeOff } from "react-icons-kit/feather/eyeOff";
-import { eye } from "react-icons-kit/feather/eye";
-import { useLogin } from "../hooks/useLogin";
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { Icon } from 'react-icons-kit'
+import { eyeOff } from 'react-icons-kit/feather/eyeOff'
+import { eye } from 'react-icons-kit/feather/eye'
+import { useLogin } from '../hooks/useLogin'
+import OAuth2Login from 'react-simple-oauth2-login'
+import { useDispatch } from 'react-redux'
+import { setLogin } from '../store/authReducer'
+
+import { FaFacebook } from 'react-icons/fa'
+import { FcGoogle } from 'react-icons/fc'
 
 // TODO: keep log in for a longer time
 const Login = () => {
     // Show/hide Password
-    const [showPassword, setShowPassword] = useState(false);
+    const [showPassword, setShowPassword] = useState(false)
     const [email, setEmail] = useState('')
-    const [password, setPassword] = useState("");
+    const [password, setPassword] = useState('')
     const { login, isLoading, error } = useLogin()
     const navigate = useNavigate()
+    const dispatch = useDispatch()
 
     const togglePasswordVisibility = () => {
-        setShowPassword(!showPassword);
-    };
+        setShowPassword(!showPassword)
+    }
     // TODO: show error messagse as in SignUp
     // Login by username or email
     const handleSubmit = async (e) => {
@@ -29,14 +36,59 @@ const Login = () => {
         }
     }
 
+    const onSuccess = async (res) => {
+        try {
+            const accessToken = await res.access_token
+            const fbRes = await fetch(
+                `https://graph.facebook.com/me?fields=id,name,picture.type(large)&access_token=${accessToken}`
+            )
+            const profile = await fbRes.json()
+
+            console.log('my profile: ', profile)
+
+            const result = await fetch(`http://localhost:3001/auth/facebook`, {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' }, // indicate the body of request is json
+                body: JSON.stringify({
+                    facebookId: profile.id,
+                    name: profile.name,
+                    email: 'email@gmail.com',
+                    username: 'username',
+                    password: 'password',
+                    //     // handle avatar
+                    //     // profilePicture: profile.picture.data.url
+                }),
+            })
+
+            const json = await result.json()
+
+            if (!result.ok) {
+                // throw Error(json.error)
+                console.log('not ok')
+            } else {
+                dispatch(
+                    setLogin({
+                        user: json.user,
+                        token: json.token,
+                    })
+                )
+            }
+
+            navigate('/')
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    const onFailure = (res) => {
+        console.log(res)
+    }
+
     return (
-        <header className="App-login">
-            <form
-                className="w-full h-screen flex justify-center items-center"
-                onSubmit={handleSubmit}
-            >
-                <div className="bg-slate-50 flex flex-col items-center gap-3 rounded-[30px] justify-center py-10 w-4/5 sm:w-2/5">
-                    <h3 style={{ fontSize: "8vh", marginTop: "2vh" }}>Log in</h3>
+        <header>
+            <div className="w-full h-screen flex justify-center items-center">
+                <div className="bg-slate-50 flex flex-col items-center gap-3 rounded-[20px] justify-center py-10 pb-16 w-4/5 sm:w-2/5">
+                    <h3 className="font-azeret text-[40px] mb-6">Log In</h3>
                     <input
                         className="textStyles outline-none"
                         placeholder="Email"
@@ -48,8 +100,8 @@ const Login = () => {
                     <div className="relative w-[70%] textStyles">
                         <input
                             className="bg-slate-50 w-[100%] outline-none"
-                            type={showPassword ? "text" : "password"}
-                            placeholder="Password"
+                            type={showPassword ? 'text' : 'password'}
+                            placeholder="Password*"
                             id="password"
                             name="password"
                             value={password}
@@ -63,34 +115,66 @@ const Login = () => {
                             <Icon icon={showPassword ? eye : eyeOff} />
                         </button>
                     </div>
-                    <div className=" w-[70%] flex h-16">
-                        {
-                            error &&
+                    {/* <div className=" w-[70%] flex h-16">
+                        {error && (
                             <div className="text-[14px] mr-auto text-red-600">
                                 {error}
                             </div>
-                        }
-                    </div>
+                        )}
+                    </div> */}
                     <button
-                        className="font-azeret bg-[#FFB302] w-[20vh] text-[large] font-bold border h-[7vh] mb-[-2vh] mt-[-7.5vh] rounded-[5px] border-solid border-[rgb(84,84,84)] hover:cursor-pointer hover:text-[white] hover:bg-[rgb(59,59,59)]"
+                        type="submit"
+                        className="font-azeret bg-[#FFB302] hover:bg-[#ffc235] w-[70%] text-[12px] font-bold  h-[6vh] shadow-sm mt-4 rounded-[5px] hover:cursor-pointer"
                         disabled={isLoading}
+                        onClick={handleSubmit}
                     >
                         Log in
                     </button>
-                    <div className="mt-[2vh] flex flex-row gap-3">
-                        <span>
-                            Don't have an account?
-                        </span>
+
+                    <div className="mt-[2vh] flex flex-row gap-3 text-[13px]">
+                        <span>Don't have an account?</span>
                         <Link to="/signup" className="text-[#FFB302]">
-                            <span>
-                                Sign up
-                            </span>
+                            <span>Sign up</span>
                         </Link>
                     </div>
-                </div>
-            </form>
-        </header>
-    );
-};
 
-export default Login;
+                    <div class="relative flex py-5 items-center w-[70%] text-[11px]">
+                        <div class="flex-grow border-t border-gray-300"></div>
+                        <span class="flex-shrink mx-4 text-gray-400">OR</span>
+                        <div class="flex-grow border-t border-gray-300"></div>
+                    </div>
+
+                    <button
+                        type="submit"
+                        className="font-azeret bg-[#3b5998] hover:bg-[#4162a8] text-white w-[70%] text-[12px] font-bold  h-[6vh] shadow-sm rounded-[5px] hover:cursor-pointer flex flex-row justify-center items-center gap-4"
+                        disabled={isLoading}
+                    >
+                        <FaFacebook size={16} />
+                        <OAuth2Login
+                            buttonText="Log in with Facebook"
+                            authorizationUrl="https://www.facebook.com/dialog/oauth"
+                            responseType="token"
+                            clientId="733349588684421"
+                            redirectUri="http://localhost:3000/"
+                            scope="public_profile"
+                            onSuccess={onSuccess}
+                            onFailure={onFailure}
+                        />
+                    </button>
+
+                    <button
+                        type="submit"
+                        className="font-azeret bg-white hover:bg-gray-200 text-gray-500 w-[70%] text-[12px] font-semibold  h-[6vh]  mb-[-2vh] border border-gray-300 border-1 rounded-[5px] hover:cursor-pointer flex flex-row justify-center items-center gap-4"
+                        disabled={isLoading}
+                        onClick={handleSubmit}
+                    >
+                        <FcGoogle size={16} />
+                        Log in with Google
+                    </button>
+                </div>
+            </div>
+        </header>
+    )
+}
+
+export default Login
