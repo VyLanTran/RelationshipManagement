8148327fa24c6fd448d319584d4f6a485c21b66c
dtnlong1 <dtnlong04@gmail.com>
2024-05-16 11:19:30 +0700

    this is a test

--- client/src/App.js
+++ client/src/App.js
@@ -17,144 +17,144 @@ import Event from './pages/Event.jsx'
 import FriendSuggestions from './pages/FriendSuggestions.jsx'
 
 // TODO: responsive to screen size
-
+//test
 function App() {
-    return (
-        <div className="app">
-            <Router>
-                <Routes>
-                    <Route
-                        exact
-                        path="/login"
-                        element={
-                            <ProtectedRoute isAuthPage={true}>
-                                <Login />
-                            </ProtectedRoute>
-                        }
-                    />
-                    <Route
-                        exact
-                        path="/signup"
-                        element={
-                            <ProtectedRoute isAuthPage={true}>
-                                <SignUp />
-                            </ProtectedRoute>
-                        }
-                    />
+	return (
+		<div className="app">
+			<Router>
+				<Routes>
+					<Route
+						exact
+						path="/login"
+						element={
+							<ProtectedRoute isAuthPage={true}>
+								<Login />
+							</ProtectedRoute>
+						}
+					/>
+					<Route
+						exact
+						path="/signup"
+						element={
+							<ProtectedRoute isAuthPage={true}>
+								<SignUp />
+							</ProtectedRoute>
+						}
+					/>
 
-                    <Route
-                        exact
-                        path="/"
-                        element={
-                            <ProtectedRoute>
-                                <Home />
-                            </ProtectedRoute>
-                        }
-                    />
+					<Route
+						exact
+						path="/"
+						element={
+							<ProtectedRoute>
+								<Home />
+							</ProtectedRoute>
+						}
+					/>
 
-                    <Route
-                        exact
-                        path="/settings"
-                        element={
-                            <ProtectedRoute>
-                                <Settings />
-                            </ProtectedRoute>
-                        }
-                    />
-                    <Route
-                        exact
-                        path="/connection/:userId"
-                        element={
-                            <ProtectedRoute>
-                                <Connection />
-                            </ProtectedRoute>
-                        }
-                    />
-                    <Route
-                        exact
-                        path="/notification"
-                        element={
-                            <ProtectedRoute>
-                                <Notification />
-                            </ProtectedRoute>
-                        }
-                    />
-                    <Route
-                        exact
-                        path="/:userId"
-                        element={
-                            <ProtectedRoute>
-                                <Profile />
-                            </ProtectedRoute>
-                        }
-                    />
-                    <Route
-                        exact
-                        path="/map"
-                        element={
-                            <ProtectedRoute>
-                                <Map />
-                            </ProtectedRoute>
-                        }
-                    />
-                    <Route
-                        exact
-                        path="/groups"
-                        element={
-                            <ProtectedRoute>
-                                <Groups />
-                            </ProtectedRoute>
-                        }
-                    />
-                    <Route
-                        exact
-                        path="/groups/:groupId"
-                        element={
-                            <ProtectedRoute>
-                                <GroupProfile />
-                            </ProtectedRoute>
-                        }
-                    />
-                    <Route
-                        exact
-                        path="/chats"
-                        element={
-                            <ProtectedRoute>
-                                <Chat />
-                            </ProtectedRoute>
-                        }
-                    />
-                    <Route
-                        exact
-                        path="/diary"
-                        element={
-                            <ProtectedRoute>
-                                <Diary />
-                            </ProtectedRoute>
-                        }
-                    />
-                    <Route
-                        exact
-                        path="/events"
-                        element={
-                            <ProtectedRoute>
-                                <Event />
-                            </ProtectedRoute>
-                        }
-                    />
-                    <Route
-                        exact
-                        path="/friendSuggestions"
-                        element={
-                            <ProtectedRoute>
-                                <FriendSuggestions />
-                            </ProtectedRoute>
-                        }
-                    />
-                </Routes>
-            </Router>
-        </div>
-    )
+					<Route
+						exact
+						path="/settings"
+						element={
+							<ProtectedRoute>
+								<Settings />
+							</ProtectedRoute>
+						}
+					/>
+					<Route
+						exact
+						path="/connection/:userId"
+						element={
+							<ProtectedRoute>
+								<Connection />
+							</ProtectedRoute>
+						}
+					/>
+					<Route
+						exact
+						path="/notification"
+						element={
+							<ProtectedRoute>
+								<Notification />
+							</ProtectedRoute>
+						}
+					/>
+					<Route
+						exact
+						path="/:userId"
+						element={
+							<ProtectedRoute>
+								<Profile />
+							</ProtectedRoute>
+						}
+					/>
+					<Route
+						exact
+						path="/map"
+						element={
+							<ProtectedRoute>
+								<Map />
+							</ProtectedRoute>
+						}
+					/>
+					<Route
+						exact
+						path="/groups"
+						element={
+							<ProtectedRoute>
+								<Groups />
+							</ProtectedRoute>
+						}
+					/>
+					<Route
+						exact
+						path="/groups/:groupId"
+						element={
+							<ProtectedRoute>
+								<GroupProfile />
+							</ProtectedRoute>
+						}
+					/>
+					<Route
+						exact
+						path="/chats"
+						element={
+							<ProtectedRoute>
+								<Chat />
+							</ProtectedRoute>
+						}
+					/>
+					<Route
+						exact
+						path="/diary"
+						element={
+							<ProtectedRoute>
+								<Diary />
+							</ProtectedRoute>
+						}
+					/>
+					<Route
+						exact
+						path="/events"
+						element={
+							<ProtectedRoute>
+								<Event />
+							</ProtectedRoute>
+						}
+					/>
+					<Route
+						exact
+						path="/friendSuggestions"
+						element={
+							<ProtectedRoute>
+								<FriendSuggestions />
+							</ProtectedRoute>
+						}
+					/>
+				</Routes>
+			</Router>
+		</div>
+	)
 }
 
 export default App
