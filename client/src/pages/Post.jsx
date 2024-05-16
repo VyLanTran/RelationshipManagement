0cf33818066c0661b8aafd55192b339d3c1e21a9
Jimmykhangnguyen <jimmykhangnguyen@gmail.com>
2024-05-16 19:50:41 -0400

    Set up Pages

---
+++ client/src/pages/Post.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "../components/navbar/Navbar.jsx";
+
+const Posts = () => {
+
+    return (
+        <div className="">
+            <Navbar />
+            <div className="pt-[60px]">This is the Post page</div>
+        </div>
+    )
+}
+
+export default Posts;
