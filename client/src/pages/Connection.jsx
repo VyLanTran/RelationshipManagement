cfda69b773d3cd5e2d4411b553dcec08625222f4
vytran01 <vlt002@bucknell.edu>
2024-05-20 14:55:09 -0400

    small bug fixed

--- client/src/pages/Connection.jsx
+++ client/src/pages/Connection.jsx
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import Navbar from '../components/navbar/Navbar.jsx'
-import ConnectionGroup from '../components/connection/ConnectionGroup.tsx'
+import ConnectionGroup from '../components/connection/ConnectionGroup.jsx'
+// import ConnectionCard from '../components/groups/ConnectionCard.jsx'
 import ConnectionCard from '../components/connection/ConnectionCard.jsx'
+
 import AddConnectionForm from '../components/connection/AddConnectionForm.jsx'
 import ConnectionDetail from '../components/connection/ConnectionDetailCard.jsx'
 import axios from 'axios'
@@ -86,98 +88,96 @@ const Connection = () => {
 			)
 		: []
 
-	return (
-		<div>
-			<Navbar />
-			{connections?.length == 0 ? (
-				<div className="flex justify-center pt-[10vh]">
-					<ConnectionGroup />
-					<div className="w-[145vh] rounded-[20px] h-[84vh] p-[1vh] m-[2vh]">
-						<form className="flex justify-between">
-							<div className="flex">
-								<input
-									value={searchInput}
-									onChange={searchConnection}
-									className="border border-solid border-[rgb(84,84,84)] bg-slate-50 w-[40vh] h-[6vh] outline-none rounded-3xl pl-[2vh]"
-									placeholder="Finding someone?"></input>
-								<button className="font-azeret bg-slate-50 w-[25vh] text-[large] font-bold border h-[6vh] rounded-3xl ml-[2vh] border-solid border-[rgb(84,84,84)] hover:cursor-pointer hover:text-[white] hover:bg-[rgb(59,59,59)]">
-									Search
-								</button>
-							</div>
-							<button
-								className="font-azeret bg-[#8DC363] w-[20vh] text-[large] font-bold border h-[6vh] rounded-3xl ml-[2vh] border-solid border-[rgb(84,84,84)] hover:cursor-pointer hover:text-[white] hover:bg-[rgb(59,59,59)]"
-								onClick={(e) => {
-									e.preventDefault()
-									setShowAddForm(true)
-								}}>
-								Add
-							</button>
-						</form>
-						<AddConnectionForm
-							isOpen={showAddForm}
-							onClose={handleCloseForm}
-							userId={userId}
-						/>
-					</div>
-				</div>
-			) : (
-				<div className="flex justify-center pt-[10vh]">
-					<ConnectionGroup />
-					<div className="w-[145vh] rounded-[20px] h-[84vh] p-[1vh] m-[2vh]">
-						<form className="flex justify-between">
-							<div className="flex">
-								<input
-									value={searchInput}
-									onChange={searchConnection}
-									className="border border-solid border-[rgb(84,84,84)] bg-slate-50 w-[40vh] h-[6vh] outline-none rounded-3xl pl-[2vh]"
-									placeholder="Finding someone?"></input>
-								<button className="font-azeret bg-slate-50 w-[25vh] text-[large] font-bold border h-[6vh] rounded-3xl ml-[2vh] border-solid border-[rgb(84,84,84)] hover:cursor-pointer hover:text-[white] hover:bg-[rgb(59,59,59)]">
-									Search
-								</button>
-							</div>
-							<button
-								className="font-azeret bg-[#8DC363] w-[20vh] text-[large] font-bold border h-[6vh] rounded-3xl ml-[2vh] border-solid border-[rgb(84,84,84)] hover:cursor-pointer hover:text-[white] hover:bg-[rgb(59,59,59)]"
-								onClick={(e) => {
-									e.preventDefault()
-									setShowAddForm(true)
-								}}>
-								Add
-							</button>
-						</form>
-						<AddConnectionForm
-							isOpen={showAddForm}
-							onClose={handleCloseForm}
-							userId={userId}
-						/>
-						<section className="h-[75vh] mt-[2vh] rounded-[20px]">
-							<div className="flex justify-between flex-wrap overflow-auto max-h-[100%] overflow-x-hidden">
-								{filteredData.slice().map((connection, id) => (
-									<ConnectionCard
-										key={id}
-										_id={connection['_id']}
-										name={connection['name']}
-										member_of={['Viet Tech', 'Team4'].join(', ')}
-										phone={connection['phone']}
-										email={connection['email']}
-										last_contacted={'02/04/2023'}
-										userId={userId}
-										onSelect={() => handleSelectConnection(connection)}
-									/>
-								))}
-							</div>
-						</section>
-						{showConnectionDetails && selectedConnection && (
-							<ConnectionDetail
-								isOpen={showConnectionDetails}
-								onClose={handleCloseDetail}
-								connection={selectedConnection}
-							/>
-						)}
-					</div>
-				</div>
-			)}
-		</div>
-	)
+    return (
+        <div>
+            <Navbar />
+            {connections?.length == 0 ? (
+                <div className="flex justify-center pt-[10vh]">
+                    <ConnectionGroup />
+                    <div className="w-[145vh] rounded-[20px] h-[84vh] p-[1vh] m-[2vh]">
+                        <form className="flex justify-between">
+                            <div className="flex">
+                                <input
+                                    value={searchInput}
+                                    onChange={searchConnection}
+                                    className="border border-solid border-[rgb(84,84,84)] bg-slate-50 w-[40vh] h-[6vh] outline-none rounded-3xl pl-[2vh]"
+                                    placeholder="Finding someone?"
+                                ></input>
+                                <button className="font-azeret bg-slate-50 w-[25vh] text-[large] font-bold border h-[6vh] rounded-3xl ml-[2vh] border-solid border-[rgb(84,84,84)] hover:cursor-pointer hover:text-[white] hover:bg-[rgb(59,59,59)]">
+                                    Search
+                                </button>
+                            </div>
+                            <button
+                                className="font-azeret bg-[#8DC363] w-[20vh] text-[large] font-bold border h-[6vh] rounded-3xl ml-[2vh] border-solid border-[rgb(84,84,84)] hover:cursor-pointer hover:text-[white] hover:bg-[rgb(59,59,59)]"
+                                onClick={(e) => {
+                                    e.preventDefault()
+                                    setShowAddForm(true)
+                                }}
+                            >
+                                Add
+                            </button>
+                        </form>
+                        <AddConnectionForm
+                            isOpen={showAddForm}
+                            onClose={handleCloseForm}
+                            userId={userId}
+                        />
+                    </div>
+                </div>
+            ) : (
+                <div className="flex justify-center pt-[10vh]">
+                    <ConnectionGroup total={connections.length} />
+                    <div className="w-[145vh] rounded-[20px] h-[84vh] p-[1vh] m-[2vh]">
+                        <form className="flex justify-between">
+                            <div className="flex">
+                                <input
+                                    value={searchInput}
+                                    onChange={searchConnection}
+                                    className="border border-solid border-[rgb(84,84,84)] bg-slate-50 w-[40vh] h-[6vh] outline-none rounded-3xl pl-[2vh]"
+                                    placeholder="Finding someone?"
+                                ></input>
+                                <button className="font-azeret bg-slate-50 w-[25vh] text-[large] font-bold border h-[6vh] rounded-3xl ml-[2vh] border-solid border-[rgb(84,84,84)] hover:cursor-pointer hover:text-[white] hover:bg-[rgb(59,59,59)]">
+                                    Search
+                                </button>
+                            </div>
+                            <button
+                                className="font-azeret bg-[#8DC363] w-[20vh] text-[large] font-bold border h-[6vh] rounded-3xl ml-[2vh] border-solid border-[rgb(84,84,84)] hover:cursor-pointer hover:text-[white] hover:bg-[rgb(59,59,59)]"
+                                onClick={(e) => {
+                                    e.preventDefault()
+                                    setShowAddForm(true)
+                                }}
+                            >
+                                Add
+                            </button>
+                        </form>
+                        <AddConnectionForm
+                            isOpen={showAddForm}
+                            onClose={handleCloseForm}
+                            userId={userId}
+                        />
+                        <section className="h-[75vh] mt-[2vh] rounded-[20px]">
+                            <div className="flex justify-between flex-wrap overflow-auto max-h-[100%] overflow-x-hidden">
+                                {filteredData.slice().map((connection, id) => (
+                                    <ConnectionCard
+                                        key={id}
+                                        _id={connection['_id']}
+                                        name={connection['name']}
+                                        member_of={['Viet Tech', 'Team4'].join(
+                                            ', '
+                                        )}
+                                        phone={connection['phone']}
+                                        email={connection['email']}
+                                        last_contacted={'02/04/2023'}
+                                        userId={userId}
+                                    />
+                                ))}
+                            </div>
+                        </section>
+                    </div>
+                </div>
+            )}
+        </div>
+    )
 }
 
 export default Connection
