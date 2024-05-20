6ab200637fed76e359ce837f837eb34497686c7b
Vy Tran <85639418+VyLanTran@users.noreply.github.com>
2024-05-20 15:10:57 -0400

    Update useLogin.js

--- client/src/hooks/useLogin.js
+++ client/src/hooks/useLogin.js
@@ -1,77 +1,77 @@
 import { useState } from 'react'
 import { useDispatch } from 'react-redux'
 import { setLogin } from '../store/authReducer'
-import BASE_URL from '../constants.js'
+import BASE_URL from '@/../../constants.js'
 
 export const useLogin = () => {
-	const [error, setError] = useState(null) // at the beginning, there is no error
-	const [isLoading, setIsLoading] = useState(false)
-	const dispatch = useDispatch()
+    const [error, setError] = useState(null) // at the beginning, there is no error
+    const [isLoading, setIsLoading] = useState(false)
+    const dispatch = useDispatch()
 
-	const login = async (email, password) => {
-		setIsLoading(true)
-		setError(null)
+    const login = async (email, password) => {
+        setIsLoading(true)
+        setError(null)
 
-		const res = await fetch(`${BASE_URL}/auth/login`, {
-			method: 'POST',
-			headers: { 'Content-Type': 'application/json' }, // indicate the body of request is json
-			body: JSON.stringify({
-				email,
-				password,
-			}), // convert json-formatted data into a string and parse it to extract required field
-		})
+        const res = await fetch(`${BASE_URL}/auth/login`, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' }, // indicate the body of request is json
+            body: JSON.stringify({
+                email,
+                password,
+            }), // convert json-formatted data into a string and parse it to extract required field
+        })
 
-		const json = await res.json()
+        const json = await res.json()
 
-		if (!res.ok) {
-			setIsLoading(false) // stop loading
-			setError(json.error)
-			throw new Error(json.error)
-		} else {
-			dispatch(
-				setLogin({
-					user: json.user,
-					token: json.token,
-				})
-			)
+        if (!res.ok) {
+            setIsLoading(false) // stop loading
+            setError(json.error)
+            throw new Error(json.error)
+        } else {
+            dispatch(
+                setLogin({
+                    user: json.user,
+                    token: json.token,
+                })
+            )
 
-			setIsLoading(false)
-		}
-	}
+            setIsLoading(false)
+        }
+    }
 
-	const loginOrSignupWithGoogle = async (name, email, profilePicture) => {
-		setIsLoading(true)
-		setError(null)
+    const loginOrSignupWithGoogle = async (name, email, profilePicture) => {
+        setIsLoading(true)
+        setError(null)
 
-		const res = await fetch(`${BASE_URL}/auth/google`, {
-			method: 'POST',
-			headers: { 'Content-Type': 'application/json' }, // indicate the body of request is json
-			body: JSON.stringify({
-				name,
-				email,
-				username: name,
-				password: 'password',
-				profilePicture,
-			}), // convert json-formatted data into a string and parse it to extract required field
-		})
+        const res = await fetch(`${BASE_URL}/auth/google`, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' }, // indicate the body of request is json
+            body: JSON.stringify({
+                name,
+                email,
+                username: name,
+                password: 'password',
+                profilePicture,
+            }), // convert json-formatted data into a string and parse it to extract required field
+        })
 
-		const json = await res.json()
+        const json = await res.json()
 
-		if (!res.ok) {
-			setIsLoading(false) // stop loading
-			setError(json.error)
-			throw new Error(json.error)
-		} else {
-			dispatch(
-				setLogin({
-					user: json.user,
-					token: json.token,
-				})
-			)
+        if (!res.ok) {
+            setIsLoading(false) // stop loading
+            setError(json.error)
+            throw new Error(json.error)
+        } else {
+            dispatch(
+                setLogin({
+                    user: json.user,
+                    token: json.token,
+                })
+            )
 
-			setIsLoading(false)
-		}
-	}
+            setIsLoading(false)
+        }
+    }
 
-	return { login, loginOrSignupWithGoogle, isLoading, error }
+    return { login, loginOrSignupWithGoogle, isLoading, error }
 }
