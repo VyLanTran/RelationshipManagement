2b95b845e5d00c66fd35c9559f5b9c8ed24fa630
Vy Tran <85639418+VyLanTran@users.noreply.github.com>
2024-05-20 15:32:17 -0400

    Update Event.jsx

--- client/src/pages/Event.jsx
+++ client/src/pages/Event.jsx
@@ -1,87 +1,83 @@
-import React, { useEffect, useState } from 'react'
-import Navbar from '../components/navbar/Navbar.jsx'
-import UpcomingEvent from '../components/event/UpcomingEvent.jsx'
-import BASE_URL from '../constants.js'
+import React, { useEffect, useState } from "react";
+import Navbar from "../components/navbar/Navbar.jsx";
+import UpcomingEvent from "../components/event/UpcomingEvent.jsx";
+import BASE_URL from '@/../../constants.js'
 import { useSelector } from 'react-redux'
-import { Button } from '../components/ui/button'
-import { Calendar } from '../components/ui/calendar'
-import { MdAdd } from 'react-icons/md'
+import { Button } from "../components/ui/button"
+import { Calendar } from "../components/ui/calendar"
+import { MdAdd } from "react-icons/md";
 import axios from 'axios'
-import FullCalendar from '@fullcalendar/react'
-import DayGridPlugin from '@fullcalendar/daygrid'
-import EventDetail from '../components/event/EventDetail.jsx'
+import FullCalendar from "@fullcalendar/react";
+import DayGridPlugin from "@fullcalendar/daygrid";
+import EventDetail from "../components/event/EventDetail.jsx";
 
 const Event = () => {
-	const currentUser = useSelector((state) => state.auth.user)
-	const token = useSelector((state) => state.auth.token)
 
-	const authHeader = { headers: { Authorization: `Bearer ${token}` } }
+    const currentUser = useSelector((state) => state.auth.user)
+    const token = useSelector((state) => state.auth.token)
 
-	const [events, setEvents] = useState([])
+    const authHeader = { headers: { Authorization: `Bearer ${token}` } }
 
-	const [date, setDate] = useState(new Date())
+    const [events, setEvents] = useState([])
 
-	useEffect(() => {
-		const fetchEvents = async () => {
-			try {
-				const response = await axios.get(
-					`${BASE_URL}/events/user/${currentUser._id}`,
-					authHeader
-				)
-				setEvents(response.data)
-			} catch (err) {
-				console.error(err)
-			}
-		}
-		fetchEvents()
-	}, [])
+    const [date, setDate] = useState(new Date());
 
-	const calendarEvent = events.map(({ name, startDate, endDate }) => ({
-		title: name,
-		start: startDate,
-		end: endDate,
-		color: '#FFB302',
-	}))
+    useEffect(() => {
+        const fetchEvents = async () => {
+            try {
+                const response = await axios.get(
+                    `${BASE_URL}/events/user/${currentUser._id}`,
+                    authHeader
+                )
+                setEvents(response.data);
+            } catch (err) {
+                console.error(err);
+            }
+        }
+        fetchEvents();
+    }, [])
 
-	const eventClick = (info) => {
-		alert(info.event.title)
-		;<EventDetail event={info.event} />
-	}
+    const calendarEvent = events.map(({ name, startDate, endDate }) => ({
+        title: name,
+        start: startDate,
+        end: endDate,
+        color: '#FFB302',
+    }))
 
-	return (
-		<div>
-			<Navbar />
-			<div className="pt-[60px] flex flex-row">
-				<div className="flex flex-col place-items-center w-[20%] hidden lg:block">
-					<Button
-						variant="outline"
-						className="mx-[2vh] mt-[3vh] h-[10vh]">
-						<MdAdd
-							className="mr-[1vh]"
-							size={40}
-						/>
-						<p className="text-xl">Add Event</p>
-					</Button>
-					<Calendar
-						mode="single"
-						selected={date}
-						onSelect={setDate}
-						className="rounded-md border mx-[2vh] mt-[1vh] bg-[#FFF]"
-					/>
-					<UpcomingEvent events={events} />
-				</div>
-				<div className="bg-[#FFF] my-[2vh] h-[87vh] w-[78%] rounded-md p-[2vh]">
-					<FullCalendar
-						plugins={[DayGridPlugin]}
-						initialView="dayGridMonth"
-						events={calendarEvent}
-						height={580}
-						eventClick={eventClick}
-					/>
-				</div>
-			</div>
-		</div>
-	)
+    const eventClick = (info) => {
+        alert(info.event.title);
+        <EventDetail event={info.event}/>
+    }
+
+    return (
+        <div>
+            <Navbar />
+            <div className="pt-[60px] flex flex-row">
+                <div className="flex flex-col place-items-center w-[20%] hidden lg:block">
+                    <Button variant="outline" className="mx-[2vh] mt-[3vh] h-[10vh]">
+                        <MdAdd className="mr-[1vh]" size={40} />
+                        <p className="text-xl">Add Event</p>
+                    </Button>
+                    <Calendar
+                        mode="single"
+                        selected={date}
+                        onSelect={setDate}
+                        className="rounded-md border mx-[2vh] mt-[1vh] bg-[#FFF]"
+                    />
+                    <UpcomingEvent events={events}/>
+                </div>
+                <div className="bg-[#FFF] my-[2vh] h-[87vh] w-[78%] rounded-md p-[2vh]">
+                    <FullCalendar 
+                        plugins={[ DayGridPlugin ]}
+                        initialView="dayGridMonth"
+                        events={calendarEvent}
+                        height={580}
+                        eventClick={eventClick}
+                    />
+                </div>
+            </div>
+        </div>
+    );
 }
 
-export default Event
+export default Event;
