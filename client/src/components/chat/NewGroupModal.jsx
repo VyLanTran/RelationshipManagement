05958aacf3d2200e2eaa60b0d3db18d531661b7a
Vy Tran <85639418+VyLanTran@users.noreply.github.com>
2024-05-20 14:59:28 -0400

    Update NewGroupModal.jsx

--- client/src/components/chat/NewGroupModal.jsx
+++ client/src/components/chat/NewGroupModal.jsx
@@ -2,13 +2,13 @@ import * as React from 'react'
 import { useState } from 'react'
 import { Button } from '../ui/button'
 import {
-	Dialog,
-	DialogContent,
-	DialogDescription,
-	DialogFooter,
-	DialogHeader,
-	DialogTitle,
-	DialogTrigger,
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
 } from '../ui/dialog'
 import { Input } from '../ui/input'
 import { Label } from '../ui/label'
@@ -22,169 +22,176 @@ import { Badge } from '../ui/badge'
 import { X } from 'lucide-react'
 import { setAllChats, setCurrentChat } from '../../store/chatReducer.js'
 import { Toaster } from '../ui/toaster'
-import BASE_URL from '../../constants.js'
+import BASE_URL from '@/../../constants.js'
 
 export function NewGroupModal({ children }) {
-	const token = useSelector((state) => state.auth.token)
-	const dispatch = useDispatch()
+    const token = useSelector((state) => state.auth.token)
+    const dispatch = useDispatch()
 
-	const [chatName, setChatName] = useState('')
-	const [selectedUsers, setSelectedUsers] = useState([])
-	const [searchResult, setSearchResult] = useState([])
-	const [open, setOpen] = useState(false)
+    const [chatName, setChatName] = useState('')
+    const [selectedUsers, setSelectedUsers] = useState([])
+    const [searchResult, setSearchResult] = useState([])
+    const [open, setOpen] = useState(false)
 
-	const { toast } = useToast()
+    const { toast } = useToast()
 
-	const handleSearch = async (query) => {
-		try {
-			const res = await fetch(`${BASE_URL}/users/search/?search=${query}`, {
-				method: 'GET',
-				headers: { Authorization: `Bearer ${token}` },
-			})
-			let data = await res.json()
-			data = data.filter(
-				(user) =>
-					!selectedUsers.some((selectedUser) => user._id === selectedUser._id)
-			)
-			setSearchResult(data)
-		} catch (error) {
-			toast({
-				variant: 'destructive',
-				title: 'Uh oh! Something went wrong.',
-				description: error.message,
-				action: <ToastAction altText="Try again">Try again</ToastAction>,
-			})
-		}
-	}
+    const handleSearch = async (query) => {
+        try {
+            const res = await fetch(
+                `${BASE_URL}/users/search/?search=${query}`,
+                {
+                    method: 'GET',
+                    headers: { Authorization: `Bearer ${token}` },
+                }
+            )
+            let data = await res.json()
+            data = data.filter(
+                (user) =>
+                    !selectedUsers.some(
+                        (selectedUser) => user._id === selectedUser._id
+                    )
+            )
+            setSearchResult(data)
+        } catch (error) {
+            toast({
+                variant: 'destructive',
+                title: 'Uh oh! Something went wrong.',
+                description: error.message,
+                action: (
+                    <ToastAction altText="Try again">Try again</ToastAction>
+                ),
+            })
+        }
+    }
 
-	const handleSubmit = async (e) => {
-		e.preventDefault()
-		try {
-			const selectedUserIds = selectedUsers.map((user) => user._id)
-			const res = await fetch(`${BASE_URL}/chats/group`, {
-				method: 'POST',
-				headers: {
-					Authorization: `Bearer ${token}`,
-					'Content-Type': 'application/json',
-				},
-				body: JSON.stringify({
-					chatName,
-					memberIds: JSON.stringify(selectedUserIds),
-				}),
-			})
-			const json = await res.json()
-			if (res.ok) {
-				dispatch(setAllChats(json))
-				dispatch(setCurrentChat(json))
-				handleClose()
-			} else {
-				toast({
-					variant: 'destructive',
-					title: json.error,
-				})
-			}
-		} catch (error) {
-			// toast({
-			//   variant: 'destructive',
-			//   title: error,
-			// })
-			console.log(error)
-		}
-	}
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        try {
+            const selectedUserIds = selectedUsers.map((user) => user._id)
+            const res = await fetch(`${BASE_URL}/chats/group`, {
+                method: 'POST',
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    chatName,
+                    memberIds: JSON.stringify(selectedUserIds),
+                }),
+            })
+            const json = await res.json()
+            if (res.ok) {
+                dispatch(setAllChats(json))
+                dispatch(setCurrentChat(json))
+                handleClose()
+            } else {
+                toast({
+                    variant: 'destructive',
+                    title: json.error,
+                })
+            }
+        } catch (error) {
+            // toast({
+            //   variant: 'destructive',
+            //   title: error,
+            // })
+            console.log(error)
+        }
+    }
 
-	const addMember = (newMember) => {
-		setSelectedUsers([...selectedUsers, newMember])
-		setSearchResult(searchResult.filter((user) => user._id !== newMember._id))
-	}
+    const addMember = (newMember) => {
+        setSelectedUsers([...selectedUsers, newMember])
+        setSearchResult(
+            searchResult.filter((user) => user._id !== newMember._id)
+        )
+    }
 
-	const deleteMember = (deletedMember) => {
-		setSelectedUsers(
-			selectedUsers.filter((user) => user._id !== deletedMember._id)
-		)
-		setSearchResult([...searchResult, deletedMember])
-	}
+    const deleteMember = (deletedMember) => {
+        setSelectedUsers(
+            selectedUsers.filter((user) => user._id !== deletedMember._id)
+        )
+        setSearchResult([...searchResult, deletedMember])
+    }
 
-	// TODO: call handleClose when closing modal
-	const handleClose = () => {
-		setChatName('')
-		setSelectedUsers([])
-		setSearchResult([])
-		setOpen(false)
-	}
+    // TODO: call handleClose when closing modal
+    const handleClose = () => {
+        setChatName('')
+        setSelectedUsers([])
+        setSearchResult([])
+        setOpen(false)
+    }
 
-	return (
-		<form onSubmit={handleSubmit}>
-			<Dialog
-				open={open}
-				onOpenChange={setOpen}>
-				<DialogTrigger asChild>{children}</DialogTrigger>
+    return (
+        <form onSubmit={handleSubmit}>
+            <Dialog open={open} onOpenChange={setOpen}>
+                <DialogTrigger asChild>{children}</DialogTrigger>
 
-				<DialogContent
-					className="sm:max-w-[425px]"
-					handleClose={handleClose}>
-					<DialogHeader className="items-center">
-						<DialogTitle>Create new group chat</DialogTitle>
-					</DialogHeader>
-					<div className="flex flex-col gap-4 py-4">
-						<div className="  ">
-							<Input
-								id="chatName"
-								placeholder="Chat name"
-								className=""
-								value={chatName}
-								onChange={(e) => setChatName(e.target.value)}
-							/>
-						</div>
-						<div className="rounded-md border border-input flex flex-wrap p-2 gap-2">
-							{selectedUsers.map((selectedUser) => (
-								<Badge
-									key={selectedUser._id}
-									id={selectedUser._id}
-									variant="secondary"
-									className="bg-[#fde09c] p-2 gap-2 hover:bg-[#fde09c] inline-flex mr-1">
-									<span className="whitespace-no-wrap">
-										{selectedUser.name}
-									</span>
-									<div
-										className="rounded-full hover:bg-[#FFB302] p-0.5"
-										onClick={() => deleteMember(selectedUser)}>
-										<X
-											size={10}
-											strokeWidth={1.25}
-										/>
-									</div>
-								</Badge>
-							))}
-							<HiddenInput
-								id="members"
-								placeholder="Members"
-								className="border-none shadow-none focus:border-none focus:outline-none focus:none"
-								onChange={(e) => handleSearch(e.target.value)}
-							/>
-						</div>
+                <DialogContent
+                    className="sm:max-w-[425px]"
+                    handleClose={handleClose}
+                >
+                    <DialogHeader className="items-center">
+                        <DialogTitle>Create new group chat</DialogTitle>
+                    </DialogHeader>
+                    <div className="flex flex-col gap-4 py-4">
+                        <div className="  ">
+                            <Input
+                                id="chatName"
+                                placeholder="Chat name"
+                                className=""
+                                value={chatName}
+                                onChange={(e) => setChatName(e.target.value)}
+                            />
+                        </div>
+                        <div className="rounded-md border border-input flex flex-wrap p-2 gap-2">
+                            {selectedUsers.map((selectedUser) => (
+                                <Badge
+                                    key={selectedUser._id}
+                                    id={selectedUser._id}
+                                    variant="secondary"
+                                    className="bg-[#fde09c] p-2 gap-2 hover:bg-[#fde09c] inline-flex mr-1"
+                                >
+                                    <span className="whitespace-no-wrap">
+                                        {selectedUser.name}
+                                    </span>
+                                    <div
+                                        className="rounded-full hover:bg-[#FFB302] p-0.5"
+                                        onClick={() =>
+                                            deleteMember(selectedUser)
+                                        }
+                                    >
+                                        <X size={10} strokeWidth={1.25} />
+                                    </div>
+                                </Badge>
+                            ))}
+                            <HiddenInput
+                                id="members"
+                                placeholder="Members"
+                                className="border-none shadow-none focus:border-none focus:outline-none focus:none"
+                                onChange={(e) => handleSearch(e.target.value)}
+                            />
+                        </div>
 
-						<ScrollArea className="h-72 w-full rounded-md border">
-							<div className="p-2">
-								{searchResult.map((user) => (
-									<UserItem
-										id={user._id}
-										user={user}
-										addMember={() => addMember(user)}
-									/>
-								))}
-							</div>
-						</ScrollArea>
-					</div>
-					<DialogFooter>
-						<Button
-							className="bg-[#FFB302]"
-							onClick={handleSubmit}>
-							Save
-						</Button>
-					</DialogFooter>
-				</DialogContent>
-				<Toaster />
-			</Dialog>
-		</form>
-	)
+                        <ScrollArea className="h-72 w-full rounded-md border">
+                            <div className="p-2">
+                                {searchResult.map((user) => (
+                                    <UserItem
+                                        id={user._id}
+                                        user={user}
+                                        addMember={() => addMember(user)}
+                                    />
+                                ))}
+                            </div>
+                        </ScrollArea>
+                    </div>
+                    <DialogFooter>
+                        <Button className="bg-[#FFB302]" onClick={handleSubmit}>
+                            Save
+                        </Button>
+                    </DialogFooter>
+                </DialogContent>
+                <Toaster />
+            </Dialog>
+        </form>
+    )
 }
