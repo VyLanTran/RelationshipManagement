d627095888f4a3c983baf0ee6d1cefc89a28667b
Vy Tran <85639418+VyLanTran@users.noreply.github.com>
2024-05-20 14:58:08 -0400

    Update CurrentChat.jsx

--- client/src/components/chat/CurrentChat.jsx
+++ client/src/components/chat/CurrentChat.jsx
@@ -19,229 +19,233 @@ const ENDPOINT = `${BASE_URL}`
 var socket, currentChatCompare
 
 const CurrentChat = () => {
-	const dispatch = useDispatch()
+    const dispatch = useDispatch()
 
-	const currentChat = useSelector((state) => state.chat.currentChat)
-	const unreadChats = useSelector((state) => state.chat.unreadChats)
-	const token = useSelector((state) => state.auth.token)
-	const currentUser = useSelector((state) => state.auth.user)
+    const currentChat = useSelector((state) => state.chat.currentChat)
+    const unreadChats = useSelector((state) => state.chat.unreadChats)
+    const token = useSelector((state) => state.auth.token)
+    const currentUser = useSelector((state) => state.auth.user)
 
-	const [input, setInput] = useState('')
-	const [messages, setMessages] = useState([])
-	const [isLoading, setIsLoading] = useState(false)
-	const [socketConnected, setSocketConnected] = useState(false)
-	const [typing, setTyping] = useState(false)
-	const [isTyping, setIsTyping] = useState(false)
+    const [input, setInput] = useState('')
+    const [messages, setMessages] = useState([])
+    const [isLoading, setIsLoading] = useState(false)
+    const [socketConnected, setSocketConnected] = useState(false)
+    const [typing, setTyping] = useState(false)
+    const [isTyping, setIsTyping] = useState(false)
 
-	const { toast } = useToast()
+    const { toast } = useToast()
 
-	// set up
-	useEffect(() => {
-		socket = io(ENDPOINT)
-		// emit user information to the socket named 'setup' in backend
-		socket.emit('setup', currentUser)
-		socket.on('connected', () => setSocketConnected(true))
-		socket.on('typing', () => setIsTyping(true))
-		socket.on('stop typing', () => setIsTyping(false))
-	}, [])
+    // set up
+    useEffect(() => {
+        socket = io(ENDPOINT)
+        // emit user information to the socket named 'setup' in backend
+        socket.emit('setup', currentUser)
+        socket.on('connected', () => setSocketConnected(true))
+        socket.on('typing', () => setIsTyping(true))
+        socket.on('stop typing', () => setIsTyping(false))
+    }, [])
 
-	// join chat room
-	useEffect(() => {
-		const getAllMessages = async () => {
-			if (!currentChat) return
+    // join chat room
+    useEffect(() => {
+        const getAllMessages = async () => {
+            if (!currentChat) return
 
-			setIsLoading(true)
-			const res = await fetch(`${BASE_URL}/messages/${currentChat._id}`, {
-				method: 'GET',
-				headers: { Authorization: `Bearer ${token}` },
-			})
+            setIsLoading(true)
+            const res = await fetch(`${BASE_URL}/messages/${currentChat._id}`, {
+                method: 'GET',
+                headers: { Authorization: `Bearer ${token}` },
+            })
 
-			const json = await res.json()
+            const json = await res.json()
 
-			if (res.ok) {
-				setMessages(json)
-			} else {
-				toast({
-					variant: 'destructive',
-					title: json.error,
-				})
-			}
-			setIsLoading(false)
-			socket.emit('join chat', currentChat._id)
-		}
+            if (res.ok) {
+                setMessages(json)
+            } else {
+                toast({
+                    variant: 'destructive',
+                    title: json.error,
+                })
+            }
+            setIsLoading(false)
+            socket.emit('join chat', currentChat._id)
+        }
 
-		if (token) {
-			getAllMessages()
+        if (token) {
+            getAllMessages()
 
-			// currentChatCompare is used to decide if we have to emit a messag (sender)
-			//  or receive a notification (receiver)
-			currentChatCompare = currentChat
-		}
-	}, [currentChat])
+            // currentChatCompare is used to decide if we have to emit a messag (sender)
+            //  or receive a notification (receiver)
+            currentChatCompare = currentChat
+        }
+    }, [currentChat])
 
-	// TODO: WAITING MESSAGE IS HAVING A PROBLEM, ONLY SHOW WAITING STATE IN THE CORRECT CHAT
-	// received message
-	useEffect(() => {
-		socket.on('message received', (messageReceived) => {
-			// We will only see the new message if that message belongs to the current chat room
-			// Else, it should just go into the notification
+    // TODO: WAITING MESSAGE IS HAVING A PROBLEM, ONLY SHOW WAITING STATE IN THE CORRECT CHAT
+    // received message
+    useEffect(() => {
+        socket.on('message received', (messageReceived) => {
+            // We will only see the new message if that message belongs to the current chat room
+            // Else, it should just go into the notification
 
-			if (
-				!currentChatCompare ||
-				currentChatCompare._id !== messageReceived.chat._id
-			) {
-				// only add the chats to the unread notification once (even if multiple messages from that chat are unread)
-				if (
-					unreadChats &&
-					!unreadChats.some((chat) => chat._id === messageReceived.chat._id)
-				) {
-					const updatedUnreadChats = [...unreadChats, messageReceived.chat]
-					dispatch(setUnreadChats(updatedUnreadChats))
+            if (
+                !currentChatCompare ||
+                currentChatCompare._id !== messageReceived.chat._id
+            ) {
+                // only add the chats to the unread notification once (even if multiple messages from that chat are unread)
+                if (
+                    unreadChats &&
+                    !unreadChats.some(
+                        (chat) => chat._id === messageReceived.chat._id
+                    )
+                ) {
+                    const updatedUnreadChats = [
+                        ...unreadChats,
+                        messageReceived.chat,
+                    ]
+                    dispatch(setUnreadChats(updatedUnreadChats))
 
-					// TODO: fetch again to update latest message, which is displayed in the chat list
-					// TODO: we should only receive notification if we are not opening that chat
-				}
-			} else {
-				setMessages([...messages, messageReceived])
-			}
-		})
-	}, [messages])
+                    // TODO: fetch again to update latest message, which is displayed in the chat list
+                    // TODO: we should only receive notification if we are not opening that chat
+                }
+            } else {
+                setMessages([...messages, messageReceived])
+            }
+        })
+    }, [messages])
 
-	// new message
-	const sendMessage = async (e) => {
-		try {
-			socket.emit('stop typing', currentChat._id)
+    // new message
+    const sendMessage = async (e) => {
+        try {
+            socket.emit('stop typing', currentChat._id)
 
-			if (!input.trim()) {
-				return
-			}
+            if (!input.trim()) {
+                return
+            }
 
-			const res = await fetch(`${BASE_URL}/messages/${currentChat._id}`, {
-				method: 'POST',
-				headers: {
-					Authorization: `Bearer ${token}`,
-					'Content-Type': 'application/json',
-				},
-				body: JSON.stringify({
-					content: input,
-				}),
-			})
-			const json = await res.json()
-			if (res.ok) {
-				socket.emit('new message', json)
-				setMessages([...messages, json])
-				setInput('')
-			} else {
-				toast({
-					variant: 'destructive',
-					title: json.error,
-				})
-			}
-		} catch (error) {
-			// console.log(error)
-			toast({
-				variant: 'destructive',
-				title: error,
-				// title: 'not ok 2',
-			})
-		}
-	}
+            const res = await fetch(`${BASE_URL}/messages/${currentChat._id}`, {
+                method: 'POST',
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    content: input,
+                }),
+            })
+            const json = await res.json()
+            if (res.ok) {
+                socket.emit('new message', json)
+                setMessages([...messages, json])
+                setInput('')
+            } else {
+                toast({
+                    variant: 'destructive',
+                    title: json.error,
+                })
+            }
+        } catch (error) {
+            // console.log(error)
+            toast({
+                variant: 'destructive',
+                title: error,
+                // title: 'not ok 2',
+            })
+        }
+    }
 
-	const handleOnChange = (e) => {
-		setInput(e.target.value)
+    const handleOnChange = (e) => {
+        setInput(e.target.value)
 
-		if (!socketConnected) return
-		if (!typing) {
-			setTyping(true)
-			socket.emit('typing', currentChat._id)
-		}
-		let lastTypingTime = new Date().getTime()
-		// If user stops typing for more than 3 seconds, then typing becomes false
-		var timerLength = 3000
-		setTimeout(() => {
-			var timeNow = new Date().getTime()
-			var timeDiff = timeNow - lastTypingTime
-			if (timeDiff >= timerLength && typing) {
-				// Emit the 'stop typing' event
-				socket.emit('stop typing', currentChat._id)
-				setTyping(false)
-			}
-		}, timerLength)
-	}
+        if (!socketConnected) return
+        if (!typing) {
+            setTyping(true)
+            socket.emit('typing', currentChat._id)
+        }
+        let lastTypingTime = new Date().getTime()
+        // If user stops typing for more than 3 seconds, then typing becomes false
+        var timerLength = 3000
+        setTimeout(() => {
+            var timeNow = new Date().getTime()
+            var timeDiff = timeNow - lastTypingTime
+            if (timeDiff >= timerLength && typing) {
+                // Emit the 'stop typing' event
+                socket.emit('stop typing', currentChat._id)
+                setTyping(false)
+            }
+        }, timerLength)
+    }
 
-	const defaultOptions = {
-		loop: true,
-		autoplay: true,
-		animationData: animationData,
-		rendererSettings: {
-			preserveAspectRatio: 'xMidYMid slice',
-		},
-	}
+    const defaultOptions = {
+        loop: true,
+        autoplay: true,
+        animationData: animationData,
+        rendererSettings: {
+            preserveAspectRatio: 'xMidYMid slice',
+        },
+    }
 
-	return (
-		<div className="w-full flex flex-col h-full">
-			{currentChat ? (
-				<div>
-					<div className="h-[60px] w-full flex flex-row justify-between items-center p-4 border-b fixed  bg-white">
-						<span>{currentChat.chatName}</span>
-					</div>
+    return (
+        <div className="w-full flex flex-col h-full">
+            {currentChat ? (
+                <div>
+                    <div className="h-[60px] w-full flex flex-row justify-between items-center p-4 border-b fixed  bg-white">
+                        <span>{currentChat.chatName}</span>
+                    </div>
 
-					<div className="flex flex-col overflow-y-auto h-full bg-white mt-[60px] py-4">
-						{isLoading ? (
-							<Spinner />
-						) : (
-							<div>
-								{messages.map((message, index) => (
-									<div
-										id={message._id}
-										className="w-full">
-										<MessageItem
-											index={index}
-											user={message.sender}
-											message={message}
-											messages={messages}
-										/>
-									</div>
-								))}
-							</div>
-						)}
+                    <div className="flex flex-col overflow-y-auto h-full bg-white mt-[60px] py-4">
+                        {isLoading ? (
+                            <Spinner />
+                        ) : (
+                            <div>
+                                {messages.map((message, index) => (
+                                    <div id={message._id} className="w-full">
+                                        <MessageItem
+                                            index={index}
+                                            user={message.sender}
+                                            message={message}
+                                            messages={messages}
+                                        />
+                                    </div>
+                                ))}
+                            </div>
+                        )}
 
-						{isTyping && (
-							<Card
-								className={`w-[60px] inline-block rounded-3xl shadow-none ml-12
-                `}>
-								<Lottie
-									options={defaultOptions}
-									className="w-full"
-								/>
-							</Card>
-						)}
-					</div>
+                        {isTyping && (
+                            <Card
+                                className={`w-[60px] inline-block rounded-3xl shadow-none ml-12
+                `}
+                            >
+                                <Lottie
+                                    options={defaultOptions}
+                                    className="w-full"
+                                />
+                            </Card>
+                        )}
+                    </div>
 
-					<div className="h-[60px] w-[70%] flex items-center fixed bottom-0 bg-white z-10 p-4">
-						<Textarea
-							placeholder="Type a message..."
-							className="h-[40px] focus:outline-none bg-gray-100"
-							value={input}
-							onChange={handleOnChange}
-							onKeyDown={(e) => {
-								// Send message when hit enter
-								// Hit Shift + Enter to go to a new line
-								if (e.key === 'Enter' && !e.shiftKey) {
-									e.preventDefault()
-									sendMessage()
-								}
-							}}
-						/>
-					</div>
+                    <div className="h-[60px] w-[70%] flex items-center fixed bottom-0 bg-white z-10 p-4">
+                        <Textarea
+                            placeholder="Type a message..."
+                            className="h-[40px] focus:outline-none bg-gray-100"
+                            value={input}
+                            onChange={handleOnChange}
+                            onKeyDown={(e) => {
+                                // Send message when hit enter
+                                // Hit Shift + Enter to go to a new line
+                                if (e.key === 'Enter' && !e.shiftKey) {
+                                    e.preventDefault()
+                                    sendMessage()
+                                }
+                            }}
+                        />
+                    </div>
 
-					<Toaster />
-				</div>
-			) : (
-				<div>Select a chat</div>
-			)}
-		</div>
-	)
+                    <Toaster />
+                </div>
+            ) : (
+                <div>Select a chat</div>
+            )}
+        </div>
+    )
 }
 
 export default CurrentChat
