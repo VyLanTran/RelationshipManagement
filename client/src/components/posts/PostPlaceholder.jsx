a55e8ffd7143793b4b0dd637695f8bf1af74d316
VyLanTran <tranlanvy1203@gmail.com>
2024-06-06 08:33:04 -0400

    add PostPlaceholder

--- client/src/components/posts/PostPlaceholder.jsx
+++ client/src/components/posts/PostPlaceholder.jsx
@@ -1,40 +1,32 @@
-import { React, useState } from "react";
-import { FaHeart } from "react-icons/fa";
+import {React, useState} from "react";
+import { FaHeart } from 'react-icons/fa';
 
-const placeholderAvatar = "/avaPlaceHolder.png";
+const placeholderAvatar = 'https://hwchamber.co.uk/wp-content/uploads/2022/04/avatar-placeholder.gif'
 
-const PostUnread = ({ user, post }) => {
-    console.log("Testing change")
-    console.log(user)
-    console.log(post)
+const PostPlaceholder = ({user, post}) => {
 
-    const likes = post.likes || {};
-    const likeCount = Object.values(likes).filter((value) => value).length;
+    const likes = post.likes || {}
+    const likeCount = Object.values(likes).filter(value => value).length
 
     return (
-        <div className="w-[80vh] h-[50vh] mt-[2vh] flex flex-col overflow-y-auto bg-slate-600">
-        <div className="bg-zinc-700 w-[40%] h-[50vh] flex flex-col items-center justify-around">
-            {user.profilePicture ? (
-            <img
-                src={user.profilePicture.url}
-                alt="avatar"
-                className="w-[15vh] h-[20vh] border-none"
-            />
-            ) : (
-            <img
-                src={placeholderAvatar}
-                alt="avatar"
-                className="w-[15vh] h-[20vh] border-none"
-            />
-            )}
-            <div>
-                <p>This is the name</p>
-                <p>This is the date</p>
-                <p>THis is the group</p>
+        <div className="w-[70vh] mt-[2vh] rounded-[20px]  flex flex-col h-auto overflow-y-auto ">
+            <div className="author flex flex-row ml-0">
+                {user.profilePicture ? <img src={user.profilePicture.url} alt="avatar" className="avatar w-[5vh] h-[5vh] m-2 rounded-[20px] border-none" /> : <img src={placeholderAvatar} alt="avatar" className="avatar w-[5vh] h-[5vh] m-2 rounded-[20px] border-none" />}
+                <div className="user font-bold mt-[2vh]"> {user.username} </div>
             </div>
-        </div>
-        </div>
-    );
-};
+            <div className="imageContainer">
+                <img src={post.picture} className="w-full rounded-[7px]"/>
+            </div>
+            <div className="text-left text-base m-[1vh]">
+                <p>{post.description}</p>
+            </div>
+            <div className="ml-[1vh] mt-[1vh] flex flex-row">
+                <FaHeart size={20}  />
+                <p className="ml-2 text-base">{likeCount}</p>
+            </div>
+            <hr className="border-t-2 border-grey my-4"></hr>
+        </div>    
+    )
+}
 
-export default PostUnread;
+export default PostPlaceholder; 
